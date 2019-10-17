4d230d5fcb1330a0fa7c2bc9527840ac92bbbcfd
JackWilb <jackwilburn@tutanota.com>
2019-10-17 10:29:43 -0600

    only run rebuild on closes and cleanup the check for updates script

--- check-for-updates.sh
+++ check-for-updates.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+# Define variables
+REPO=$1
+LOCATION=/home/ec2-user/multinet-deploy
+
 # Add an option to let us parse the .env
 set +H
 
 # Move to directory
-cd /home/ec2-user/multinet-deploy/
+cd $LOCATION
 
 # Clean out all old builds, rebuild, and deploy
+# If the repo is multinet we need to rebuild the client and server
+if [ $REPO == "multinet" ]
+then
 docker system prune --all -f
-docker-compose build --no-cache
+docker-compose build --no-cache $REPO-client
+docker-compose build --no-cache $REPO-server
 docker-compose up -d
+else
+# Build the one container
+docker system prune --all -f
+docker-compose build --no-cache $REPO
+docker-compose up -d
+fi
 
-echo "run successful "$(date) >> /home/ec2-user/multinet-deploy/whook.log
+echo $REPO" run successful "$(date) >> $LOCATION/whook.log
 
 # mailbody="Testmail via bash script"
 # echo "From: info@myserver.test" > /tmp/mailtest
--- webhooks/app.js
+++ webhooks/app.js
@@ -6,17 +6,37 @@ const secret = process.env.WEBHOOK_SECRET
 
 http.createServer(function(request, response) {
     request.on('data', function(chunk) {
+        // Get the data that came in and grab a couple things we need
+        data = JSON.parse(chunk.toString())
+        repo = data.repository.name
+        action = data.action
+
+        // Get make sure the signature matches our secret
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-        if (crypto.timingSafeEqual(Buffer.from(request.headers['x-hub-signature']), Buffer.from(sig))) {
-            exec(
-                "/home/ec2-user/multinet-deploy/check-for-updates.sh"
-            )
+        if (request.headers['x-hub-signature']) {
+            if (crypto.timingSafeEqual(Buffer.from(request.headers['x-hub-signature']), Buffer.from(sig))) {
+                // Check what type of action we're getting
+                if (action = "close") {
+                    // Call the script
+                    exec("/home/ec2-user/multinet-deploy/check-for-updates.sh " + repo)
+
+                    // Respond with successful call
+                    response.writeHead(200, { 'Content-type': 'text/html' });
+                    response.write("Received a " + data.action + " request from the " + repo + " repository.");
+                    response.end();
+                } else {
+                    // If it's not a close then do nothing
+                    response.writeHead(405, { 'Content-type': 'text/html' });
+                    response.write("Received a " + data.action + " request from the " + repo + " repository. This is not supported since the containers should only rebuild on a close.");
+                    response.end();
+                }
+            }
+        } else {
+            // Respond with not authorized
+            response.writeHead(400, { 'Content-type': 'text/html' });
+            response.write('Incorrect auth token.');
+            response.end();
         }
-        response.writeHead(200, { 'Content-type': 'text/json' });
-        response.write('Successful run');
-        response.end();
+
     })
-
-
-
 }).listen(4112);
